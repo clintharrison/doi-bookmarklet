ce6de91117f65ede39aeae871f174c762a3f10c1
Clint Harrison <clinteger@gmail.com>
2012-08-05 00:49:56 -0400

    Add bookmarklet JavaScript source

--- doi-bookmarklet.js
+++ doi-bookmarklet.js
@@ -1,12 +1,12 @@
 (function() {
-    var proxyUrl = "";
+    var proxyUrl = "http://proxy.lib.umich.edu";
     var selection = document.getSelection ? document.getSelection().toString() : document.selection.createRange().text;
     var regex = /\w?(?:doi:|hdl:(?:\/\/)?)\s*([0-9./a-z]+)\w?/i;
     var match = regex.exec(selection);
     if (match != null) {
-	var doi = match[1];
-	var tempAnchor = document.createElement('a');
-	tempAnchor.href = proxyUrl;
-	document.location = tempAnchor.protocol + '//dx.doi.org.' + tempAnchor.host + '/' + doi;
+	   var doi = match[1];
+	   var tempAnchor = document.createElement('a');
+	   tempAnchor.href = proxyUrl;
+	   document.location = tempAnchor.protocol + '//dx.doi.org.' + tempAnchor.host + '/' + doi;
     }
-})();
+ })();
